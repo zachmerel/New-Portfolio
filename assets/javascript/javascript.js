a3cd683e121f2f3a33fc5ce6aa78f696d62b0e00
Zach Merel <zachmerel@gmail.com>
2018-12-05 17:42:38 -0600

    elements for the main page of the portfolio are present but not aligned properly. Still need to link to a page with to show work.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //click function for LinkedIn
+    $("#linkedIn").click(function () {
+        window.location = "https://www.linkedin.com/in/zach-merel-35a46b93/";
+        console.log("click works")
+    });
+    //Click function for Github
+    $("#github").click(function () {
+        window.location = "https://github.com/zachmerel";
+        console.log("click works")
+    });
+});
