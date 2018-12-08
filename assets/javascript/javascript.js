fb9f5049d4a353997f41a921ad9ae9ebe06d3e7a
Zach Merel <zachmerel@gmail.com>
2018-12-07 22:37:37 -0600

    portfolio and about page work no contact page as of now, portfolio cards link to github pages

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,4 +9,20 @@ $(document).ready(function () {
         window.location = "https://github.com/zachmerel";
         console.log("click works")
     });
+    //Click function for Word Guess Game
+    $("#WGG-Card").click(function () {
+        window.location = "https://zachmerel.github.io/Word_Guess_Game/";
+        console.log("click works")
+    });
+    //Click function for Project 1: Doppelgangers
+    $("#Doppelgangers-Card").click(function () {
+        window.location = "https://nueva-team-blue.github.io/Project-1/";
+        console.log("click works")
+    });
+    //Click function for LOTR RPG
+    $("#LOTR-Card").click(function () {
+        window.location = "https://zachmerel.github.io/unit-4-game/";
+        console.log("click works")
+    });
+
 });
